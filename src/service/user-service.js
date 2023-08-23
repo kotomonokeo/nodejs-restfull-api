c78099455e7811fb6d571010dd9e1b9bfdd95d69
kotomonokeo <kotomonokeo@gmail.com>
2023-08-23 12:45:39 +0700

    logout user api

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -104,7 +104,6 @@ const update = async (request) => {
     throw new ResponseError(404, "user not found");
   }
 
-
   const data = {};
   if (user.name) {
     data.name = user.name;
@@ -125,9 +124,39 @@ const update = async (request) => {
   });
 };
 
+const logout = async (username) => {
+  username = validate(getUserValidation, username);
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      username: username,
+    },
+    select: {
+      username: true,
+    },
+  });
+
+  if (!user) {
+    throw new ResponseError(404, "user is not found");
+  }
+
+  return prismaClient.user.update({
+    where: {
+      username: username,
+    },
+    data: {
+      token: null,
+    },
+    select: {
+      username: true,
+    },
+  });
+};
+
 export default {
   register,
   login,
   get,
   update,
+  logout,
 };
